04f7d82f7ad7fe27080f6687c9ca9652f2d6dbc8
David Kananda <davidkananda@gmail.com>
2018-01-29 11:34:17 +0000

    Yield details render test pass

---
+++ src/components/Yield.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Yield extends Component {
+  render() {
+    return (
+      <section className="yield-details">
+        <div>Yield Details</div>
+      </section>
+    );
+  }
+}
+
+export default Yield;
