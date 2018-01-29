b573ab5c4dfa40138ed4e1d4a2cea569d72f3114
David Kananda <davidkananda@gmail.com>
2018-01-29 11:02:58 +0000

    crop list item on click test fail

--- src/tests/components/CropsList.test.js
+++ src/tests/components/CropsList.test.js
@@ -1,5 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Enzyme, { shallow } from "enzyme";
+import sinon from "sinon";
+import Adapter from "enzyme-adapter-react-16";
+
+Enzyme.configure({ adapter: new Adapter() });
 
 import CropsList from "../../components/CropsList";
 import crops from "../../data/crops.json";
@@ -7,4 +12,19 @@ import crops from "../../data/crops.json";
 it("renders crops list", () => {
   const div = document.createElement("section");
   ReactDOM.render(<CropsList crops={crops} />, div);
+});
+
+it("handles crop list item click", () => {
+  const callback = sinon.spy();
+  const wrapper = shallow(<CropsList crops={crops} onClick={callback} />);
+
+  expect(wrapper.find("ul").length).toBe(1);
+
+  wrapper
+    .find("li")
+    .first()
+    .props()
+    .onClick();
+
+  expect(callback.called).toBe(true);
 });
