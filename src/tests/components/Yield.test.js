fcfec9803a3438f4f0226a5d15be78cbf91c1d1e
David Kananda <davidkananda@gmail.com>
2018-01-29 11:33:37 +0000

    Yield import test fix. Test still failing

--- src/tests/components/Yield.test.js
+++ src/tests/components/Yield.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Yield from "../../components/CropsList";
+import Yield from "../../components/Yield";
 
 it("renders yield details", () => {
   const div = document.createElement("section");
