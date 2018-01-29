9c61fedf36159cac16ce1f9af2061e3927ca634d
David Kananda <davidkananda@gmail.com>
2018-01-29 12:33:00 +0000

    fetching UX updates + removed static farm and crops data

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 import { Map, TileLayer, GeoJSON } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
-import farm from "./data/farm.json";
-import crops from "./data/crops.json";
 import CropsList from "./components/CropsList";
 import Yield from "./components/Yield";
 
@@ -11,9 +9,13 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
+      farm: {},
+      crops: {},
       currentCrop: {},
       currentField: {},
       yieldPotential: 0,
+      isFetchingCrops: true,
+      isFetchingFarm: true,
       error: false
     };
   }
@@ -78,44 +80,60 @@ class App extends Component {
       <section className="dashboard">
         <header className="heading_dashboard">
           <h1>Farm Manager Dashboard</h1>
-          <p>
-            Welcome to the farm manager dashboard. Select a crop below to add to
-            field.
-          </p>
+          {!this.state.isFetchingCrops && !this.state.isFetchingFarm ? (
+            <p>
+              Welcome to the farm manager dashboard. Select a crop below to add
+              to field.
+            </p>
+          ) : (
+            <p>Loading Data. Please Wait</p>
+          )}
         </header>
         {this.state.error ? (
           <p className="error">Please select a crop first.</p>
         ) : null}
-        <section>
-          <Map className="map" center={farm.centre.coordinates} zoom={13}>
-            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-            {farm.fields.map(field => (
-              <GeoJSON
-                color={
-                  this.state.currentField.name === field.name ? "green" : "blue"
-                }
-                key={field.name}
-                data={field.boundary}
-                onClick={() => this.handleFieldClick(field)}
+
+        {!this.state.isFetchingCrops && !this.state.isFetchingFarm ? (
+          <section>
+            <Map
+              className="map"
+              center={this.state.farm.centre.coordinates}
+              zoom={13}
+            >
+              <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+              {this.state.farm.fields.map(field => (
+                <GeoJSON
+                  color={
+                    this.state.currentField.name === field.name
+                      ? "green"
+                      : "blue"
+                  }
+                  key={field.name}
+                  data={field.boundary}
+                  onClick={() => this.handleFieldClick(field)}
+                />
+              ))}
+            </Map>
+            <section className="content">
+              <header className="heading_yield">
+                {this.state.currentCrop.name ? (
+                  <button onClick={this.handleRemoveCrop}>
+                    Remove: {this.state.currentCrop.name}
+                  </button>
+                ) : null}
+              </header>
+              <CropsList
+                crops={this.state.crops}
+                onClick={this.handleCropClick}
               />
-            ))}
-          </Map>
-          <section className="content">
-            <header className="heading_yield">
-              {this.state.currentCrop.name ? (
-                <button onClick={this.handleRemoveCrop}>
-                  Remove: {this.state.currentCrop.name}
-                </button>
-              ) : null}
-            </header>
-            <CropsList crops={crops} onClick={this.handleCropClick} />
-            <Yield
-              yieldPotential={this.state.yieldPotential}
-              currentCrop={this.state.currentCrop}
-              currentField={this.state.currentField}
-            />
+              <Yield
+                yieldPotential={this.state.yieldPotential}
+                currentCrop={this.state.currentCrop}
+                currentField={this.state.currentField}
+              />
+            </section>
           </section>
-        </section>
+        ) : null}
       </section>
     );
   }
