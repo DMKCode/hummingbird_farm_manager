e8b310c0dda2f4de26ffc13bfb021551d900c064
David Kananda <davidkananda@gmail.com>
2018-01-29 11:45:26 +0000

    refactored yield details + passing props

--- src/components/Yield.js
+++ src/components/Yield.js
@@ -1,10 +1,43 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class Yield extends Component {
+  static propTypes = {
+    yieldPotential: PropTypes.number,
+    currentCrop: PropTypes.object,
+    currentField: PropTypes.object
+  };
+
   render() {
     return (
       <section className="yield-details">
-        <div>Yield Details</div>
+        <div>
+          <div>
+            <strong>Yield Potential: </strong>
+            <p>{this.props.yieldPotential}</p>
+          </div>
+          <h3>Yield Configuration</h3>
+          <p>
+            <strong>Crop Yield Average: </strong>
+            {this.props.currentCrop.expected_yield}
+          </p>
+          <p>
+            <strong>Hectares of field: </strong>
+            {this.props.currentField.hectares}
+          </p>
+          <p>
+            <strong>Crop Risk Factor: </strong>
+            {this.props.currentCrop.disease_risk_factor}
+          </p>
+          <p>
+            <strong>Field Disease Susceptibility: </strong>
+            {this.props.currentField.disease_susceptibility}
+          </p>
+          <p>
+            <strong>Price per tonne: </strong>
+            {this.props.currentCrop.price_per_tonne}
+          </p>
+        </div>
       </section>
     );
   }
--- src/tests/components/Yield.test.js
+++ src/tests/components/Yield.test.js
@@ -5,5 +5,8 @@ import Yield from "../../components/Yield";
 
 it("renders yield details", () => {
   const div = document.createElement("section");
-  ReactDOM.render(<Yield />, div);
+  ReactDOM.render(
+    <Yield yieldPotential={0} currentCrop={{}} currentField={{}} />,
+    div
+  );
 });
